fd29ca663c6a6d18d5ea603290debf30ae182ca2
DeepjyotiDeb <55659244+DeepjyotiDeb@users.noreply.github.com>
2025-01-23 10:34:40 +0530

    feat: fetch and display Swagger JSON dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 // import './App.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 
 function App() {
+  const [swaggerJson, setSwaggerJson] = useState(null);
+
+  useEffect(() => {
+    // fetch(apiUrl[import.meta.env.VITE_STAGE])
+    fetch('https://petstore.swagger.io/v2/swagger.json')
+      .then((response) => response.json())
+      .then((data) => setSwaggerJson(data))
+      .catch((error) => console.error('Error loading swagger json:', error));
+  }, []);
+
+  console.log('swaggerJson', swaggerJson);
   return (
     <>
-      <SwaggerUI url='https://petstore.swagger.io/v2/swagger.json' />
+      <SwaggerUI spec={swaggerJson} />
     </>
   );
 }
